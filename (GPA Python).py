555c37a97a26706b8695f8bf00917b0ef37f1c9c
GodofBeans87 <146485821+GodofBeans87@users.noreply.github.com>
2023-09-29 15:38:10 -0500

    Add files via upload
    This is a python code I made to calculate GPA.

---
+++ (GPA Python).py
@@ -0,0 +1,17 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> GPA1=6
+>>> GPA2=4
+>>> GPA3=3
+>>> Credits1=3
+>>> Credits2=3
+>>> Credits3=1
+>>> TotalGPA=GPA1+GPA2+GPA3
+>>> Totalcredits=Credits1+Credits2+Credits3
+>>> TotalGPA/Totalcredits=CumiliativeGPA
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> CumiliativeGPA=TotalGPA/Totalcredits
+>>> CumiliativeGPA=
+SyntaxError: incomplete input
+>>> CumiliativeGPA
+1.8571428571428572
