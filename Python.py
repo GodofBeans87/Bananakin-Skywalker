2d0a536c7104b377eb6effe436c0af4fdbb0d50b
GodofBeans87 <146485821+GodofBeans87@users.noreply.github.com>
2023-09-29 15:31:44 -0500

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,18 @@
+
+
+>>> input('Andrew:')
+Andrew:
+''
+>>> name=input('Enter your name:')
+Enter your name:name
+>>> name
+'name'
+>>> Andrew
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    Andrew
+NameError: name 'Andrew' is not defined
+
+name=input('Enter your name: Andrew')
+
+
